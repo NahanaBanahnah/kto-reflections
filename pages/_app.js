67d2d8d61da8d0b4329695d5711cf92ee3d77370
Nahana <37905054+NahanaBanahnah@users.noreply.github.com>
2022-05-04 13:44:49 -0500

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import '../styles/globals.css'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+		primary: {
+			main: '#772a54',
+		},
+		secondary: {
+			main: '#d79162',
+		},
+	},
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	)
 }
 
 export default MyApp

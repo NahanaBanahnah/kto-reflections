78189ef5e9e8e5a3ed5f6f44196407f7f4f7f8da
Nahana <37905054+NahanaBanahnah@users.noreply.github.com>
2023-03-12 19:52:51 -0500

    negative fix?

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,9 @@ import {
 	AppBar,
 	Divider,
 	Grid,
+	Icon,
+	IconButton,
+	Modal,
 	Table,
 	TableBody,
 	TableCell,
@@ -29,6 +32,8 @@ import {
 	Typography,
 } from '@mui/material'
 import { Box, Container, Stack } from '@mui/system'
+import InfoIcon from '@mui/icons-material/Info'
+import CloseIcon from '@mui/icons-material/Close'
 
 const Index = () => {
 	const DIVISOR = 1000000000
@@ -41,6 +46,7 @@ const Index = () => {
 	const [outs, setOuts] = useState(false)
 	const [reflections, setReflections] = useState(false)
 	const [TXS, setTxs] = useState(null)
+	const [open, setOpen] = useState(false)
 
 	const {
 		handleSubmit,
@@ -93,33 +99,49 @@ const Index = () => {
 		})
 
 		//loop the transactions, add them up, and pull the data we want
-		let total = 0
 		let totalIn = 0
 		let totalOut = 0
 		let txs = []
 
-		TXNS.data.result.forEach(e => {
-			total =
-				wallet.toLowerCase() === e.to
-					? total + parseInt(e.value)
-					: total - parseInt(e.value)
-
-			let type = wallet.toLowerCase() === e.to ? 'in' : 'out'
-
-			if (type === 'in') {
-				totalIn = totalIn + parseInt(e.value)
-			}
-			if (type === 'out') {
-				totalOut = totalOut + parseInt(e.value)
-			}
+		TXNS.data.result.forEach(item => {
+			let type = wallet.toLowerCase() === item.to ? 'in' : 'out'
+			let amount = parseInt(item.value)
 
 			let obj = {
-				timestamp: e.timeStamp,
-				amount: e.value,
-				hash: e.hash,
+				timestamp: item.timeStamp,
+				amount: amount,
+				hash: item.hash,
 				type: type,
 			}
+
+			let filterIndex = txs.findIndex(row => row.hash === item.hash)
+			if (filterIndex > 0) {
+				if (txs[filterIndex].amount < amount) {
+					let original = (txs[filterIndex].amount * 100) / 7
+					let reflectTax = original - txs[filterIndex].amount - amount
+					let txObject = {
+						timestamp: item.timeStamp,
+						amount: reflectTax,
+						hash: item.hash,
+						type: type,
+						missingTax: true,
+						original: original,
+					}
+					txs.push(txObject)
+				}
+			}
+			// } else {
 			txs.push(obj)
+			// }
+		})
+
+		txs.forEach(item => {
+			if (item.type === 'in') {
+				totalIn = totalIn + item.amount
+			}
+			if (item.type === 'out') {
+				totalOut = totalOut + item.amount
+			}
 		})
 
 		// ========== final math and set all the states
@@ -142,6 +164,21 @@ const Index = () => {
 		borderRadius: 10,
 	}))
 
+	const style = {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: `60vw`,
+		bgcolor: 'background.paper',
+		border: '2px solid #000',
+		boxShadow: 24,
+		p: 4,
+	}
+
+	const handleOpen = () => setOpen(true)
+	const handleClose = () => setOpen(false)
+
 	return (
 		<>
 			<Head>
@@ -149,13 +186,45 @@ const Index = () => {
 			</Head>
 			<ViewSource />
 
-			<Box className="topBar" sx={{ padding: 4 }}>
+			<Modal open={open} onClose={handleClose}>
+				<Box sx={style}>
+					<IconButton
+						sx={{ position: 'absolute', right: '32px' }}
+						onClick={handleClose}
+					>
+						<CloseIcon />
+					</IconButton>
+
+					<Typography variant="h6" component="h2">
+						ABOUT
+					</Typography>
+					<Typography sx={{ mt: 2 }}>
+						This tool is for informational purposes only, and should
+						not be viewed as financial advice, or suggestions.
+					</Typography>
+					<Typography sx={{ mt: 2 }}>
+						This was created to help calculate your received
+						reflections. While it should be accurate, the most
+						accurate way to calculate reflections is to keep your
+						own historical data of purchase and swap amounts.
+					</Typography>
+					<Typography sx={{ mt: 2 }}>
+						Data is pulled from Etherscan and is not stored to any
+						internal or external databases or sources.
+					</Typography>
+					<Typography sx={{ mt: 2 }}>
+						This tool is created as a community project, and is not
+						officially part of Kounotori Token.
+					</Typography>
+				</Box>
+			</Modal>
+			{/* <Box className="topBar" sx={{ padding: 4 }}>
 				<Typography variant="body1">
 					There is a currently a bug showing certain wallets a
 					negative value. This is just a visual bug, you still have
 					your reflections!
 				</Typography>
-			</Box>
+			</Box> */}
 			<Container fixed>
 				<Stack mt={4} spacing={2}>
 					<Box display="flex" justifyContent="center">
@@ -381,6 +450,14 @@ const Index = () => {
 
 			<footer className={styles.footer}>
 				Made With <FavoriteIcon className={styles.heart} /> By Nahana
+				<Button
+					startIcon={<InfoIcon />}
+					onClick={() => setOpen(true)}
+					size={'small'}
+					sx={{ marginLeft: 2 }}
+				>
+					More Info
+				</Button>
 			</footer>
 		</>
 	)
--- src/components/Txns/Txns.js
+++ src/components/Txns/Txns.js
@@ -2,15 +2,29 @@ import { useTheme } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined'
 import RemoveCircleOutlinedIcon from '@mui/icons-material/RemoveCircleOutlined'
+import HelpIcon from '@mui/icons-material/Help'
 
 import { DateTime } from 'luxon'
-import { Button, TableCell, TableRow } from '@mui/material'
+import {
+	Button,
+	IconButton,
+	Modal,
+	Popover,
+	TableCell,
+	TableRow,
+	Typography,
+} from '@mui/material'
 import styles from './txns.module.scss'
 import LinkIcon from '@mui/icons-material/Link'
+import { useRef, useState } from 'react'
+import { Box } from '@mui/system'
+import CloseIcon from '@mui/icons-material/Close'
 
 const Txns = ({ obj, divisor }) => {
 	const theme = useTheme()
 	const matches = useMediaQuery(theme.breakpoints.up('md'))
+	const [modalView, setModal] = useState(false)
+	const popRef = useRef(null)
 
 	const NUMBER_CLASS = [styles.number]
 	const ICON =
@@ -30,6 +44,13 @@ const Txns = ({ obj, divisor }) => {
 		NUMBER_CLASS.push(styles.out)
 	}
 
+	const handleOpen = () => {
+		setModal(true)
+	}
+	const handleClose = () => {
+		setModal(false)
+	}
+
 	const myDateTime = DateTime.fromSeconds(parseInt(obj.timestamp))
 	const DATE = matches
 		? myDateTime.toLocaleString({
@@ -42,12 +63,59 @@ const Txns = ({ obj, divisor }) => {
 				day: '2-digit',
 				year: '2-digit',
 		  })
+	const style = {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: `60vw`,
+		bgcolor: 'background.paper',
+		border: '2px solid #000',
+		boxShadow: 24,
+		p: 4,
+	}
 	return (
-		<TableRow>
+		<TableRow ref={popRef}>
 			<TableCell>{DATE}</TableCell>
 			<TableCell className={NUMBER_CLASS.join(' ')}>
 				{ICON}
 				{(obj.amount / divisor).toLocaleString()}
+				{obj.missingTax && (
+					<>
+						<IconButton onClick={handleOpen}>
+							<HelpIcon color="error" />
+						</IconButton>
+						<Modal open={modalView} onClose={handleClose}>
+							<Box sx={style}>
+								<IconButton
+									sx={{ position: 'absolute', right: '32px' }}
+									onClick={handleClose}
+								>
+									<CloseIcon />
+								</IconButton>
+								<Typography
+									id="modal-modal-title"
+									variant="h6"
+									component="h2"
+								>
+									What's This?
+								</Typography>
+								<Typography
+									id="modal-modal-description"
+									sx={{ mt: 2 }}
+								>
+									When you swap KTO, Etherscan only lists 2 of
+									the 3 amounts; the amount of KTO you swapped
+									out minus tax & the tax to the marketing
+									wallet. The remaining tax portion, which is
+									distributed to holders as reflections, is
+									not shown in the Etherscan txn; that is what
+									this amount is (based on a 10% sell tax)
+								</Typography>
+							</Box>
+						</Modal>
+					</>
+				)}
 			</TableCell>
 
 			<TableCell align="right">

1c939c5bdc7be6c8979d8ed2dd569fe7d8c4ba06
Nahana <37905054+NahanaBanahnah@users.noreply.github.com>
2023-03-24 17:41:56 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const path = require('path')
+
+const securityHeaders = [
+	{
+		key: 'X-Frame-Options',
+		value: 'SAMEORIGIN',
+	},
+]
+
+module.exports = {
+	reactStrictMode: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+	},
+
+	async headers() {
+		return [
+			{
+				// Apply these headers to all routes in your application.
+				source: '/(.*)',
+				headers: securityHeaders,
+			},
+		]
+	},
+}
